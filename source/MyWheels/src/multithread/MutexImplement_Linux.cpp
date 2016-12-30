4a67ce25ff78703eef2f5b974dcd324d0dd06192
SeeWhy <seewhy.chen@gmail.com>
2016-12-30 16:46:58 +0800

    check and print error info in Mutex of Linux

--- source/MyWheels/src/multithread/MutexImplement_Linux.cpp
+++ source/MyWheels/src/multithread/MutexImplement_Linux.cpp
@@ -8,23 +8,44 @@
 namespace mwl {
 
     Mutex::Implement::Implement() {
-        pthread_mutex_init(&m, NULL);
+        int32_t ret = pthread_mutex_init(&m, NULL);
+        if (ret) {
+            MWL_WARN_ERRNO("init mutex failed", ret);
+        }
     }
 
     Mutex::Implement::~Implement() {
-        pthread_mutex_destroy(&m);
+        int32_t ret = pthread_mutex_destroy(&m);
+        if (ret) {
+            MWL_WARN_ERRNO("destroy mutex failed", ret);
+        }
     }
 
     int32_t Mutex::Implement::_Lock() {
-        return -pthread_mutex_lock(&m);
+        int32_t ret = pthread_mutex_lock(&m);
+        if (ret) {
+            MWL_WARN_ERRNO("lock mutex failed", ret);
+            ret = -ret;
+        }
+        return -ret;
     }
 
     int32_t Mutex::Implement::_TryLock() {
-        return -pthread_mutex_trylock(&m);
+        int32_t ret = pthread_mutex_trylock(&m);
+        if (ret) {
+            MWL_WARN_ERRNO("trylock mutex failed", ret);
+            ret = -ret;
+        }
+        return -ret;
     }
 
     int32_t Mutex::Implement::_Unlock() {
-        return -pthread_mutex_unlock(&m);
+        int32_t ret = pthread_mutex_unlock(&m);
+        if (ret) {
+            MWL_WARN_ERRNO("unlock mutex failed", ret);
+            ret = -ret;
+        }
+        return -ret;
     }
     
 }
