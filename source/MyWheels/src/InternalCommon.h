c91e4dfb7e091019ad49635450b05288c4d3ec03
SeeWhy <seewhy.chen@gmail.com>
2017-01-10 16:12:18 +0800

    disable min/max macros in Windows

--- source/MyWheels/src/InternalCommon.h
+++ source/MyWheels/src/InternalCommon.h
@@ -7,8 +7,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <utility> // for std::swap
+#include <algorithm> // for std::min
+
 #ifdef __MWL_WIN__
 
+    #ifdef min
+    #undef min
+    #endif
+
+    #ifdef max
+    #undef max
+    #endif
+
+    #define NOMINMAX // disable macro definition of 'min'
     #include <Windows.h>
 
 #elif defined __MWL_LINUX__
